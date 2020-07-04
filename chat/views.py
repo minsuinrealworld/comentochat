2ea782095171f958b48afe91ff7888171e12a1d7
minsuinrealworld <67817871+minsuinrealworld@users.noreply.github.com>
2020-07-04 16:18:36 +0900

    Initial commit

---
+++ chat/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+def index(requests):
+    return render(requests, 'chat/index.html')
+
+@request
+def usercreate(request):
+    return JSONresponse
+
+def userinfo(request):
+    return JSONresponse
+
+def userdelete(request):
+    return JSONresponse
+
+def userupdate(request):
+    return JSONresponse
